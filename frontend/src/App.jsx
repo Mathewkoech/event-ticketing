36aa5ad72a40dcc527a319cfd141bcd0f273a393
mathewkoech <mathewkoech55@gmail.com>
2024-11-18 11:20:26 +0300

    Configure Vercel for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import Layout from './components/Layout';
 import Homepage from './components/pages/Homepage';
 import EventDetails from './components/events/EventDetails';
 import ErrorPage from './components/pages/ErrorPage';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/event/:id" element={<EventDetails />} />
         <Route path="*" element={<ErrorPage />} /> 
       </Route>
